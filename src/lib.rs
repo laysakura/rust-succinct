e211c93929a7f1561219fe58a9fdd038b8121b9f
Sho Nakatani <lay.sakura@gmail.com>
2019-03-20 08:59:07 +0900

    starts implemet succinct::BitVector

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
+pub mod succinct {
+    pub struct BitVector {
+        // internal representation
+    }
+
+    impl BitVector {
+        pub fn new(n: u64) {}
+    }
+}
+
 #[cfg(test)]
 mod build_and_access_success_tests {
+    use super::succinct::BitVector;
+
     #[test]
     fn build_works() {
-        let bv = succinct::BitVector::new(2);  // takes length
+        let bv = BitVector::new(2);  // takes length
         bv.build();
         assert_eq!(bv.access(0), false);
         assert_eq!(bv.access(1), false);
@@ -10,14 +22,14 @@ mod build_and_access_success_tests {
 
     #[test]
     fn without_build_works() {
-        let bv = succinct::BitVector::new(2);  // takes length
+        let bv = BitVector::new(2);  // takes length
         assert_eq!(bv.access(0), false);  // build() internally
         assert_eq!(bv.access(1), false);
     }
 
     #[test]
     fn build_by_set_bit() {
-        let bv = succinct::BitVector::new(2)
+        let bv = BitVector::new(2)
             .set_bit(1)
             .build();
         assert_eq!(bv.access(0), false);
@@ -26,7 +38,7 @@ mod build_and_access_success_tests {
 
     #[test]
     fn build_by_str() {
-        let bv = succinct::BitVector::new("101").build();
+        let bv = BitVector::new("101").build();
         assert_eq!(bv.access(0), true);
         assert_eq!(bv.access(1), false);
         assert_eq!(bv.access(2), true);
@@ -34,7 +46,7 @@ mod build_and_access_success_tests {
 
     #[test]
     fn build_by_str_with_set_bit() {
-        let bv = succinct::BitVector::new("101")
+        let bv = BitVector::new("101")
             .set_bit(0)
             .set_bit(1)
             .build();
@@ -46,30 +58,34 @@ mod build_and_access_success_tests {
 
 #[cfg(test)]
 mod build_and_access_failure_tests {
+    use super::succinct::BitVector;
+
     #[test]
     #[should_panic]
     fn access_over_lower_bound_causes_panic() {
-        let bv = succinct::BitVector::new(2).build();
+        let bv = BitVector::new(2).build();
         let _ = bv.access(-1);
     }
 
     #[test]
     #[should_panic]
     fn access_over_upper_bound_causes_panic() {
-        let bv = succinct::BitVector::new(2).build();
+        let bv = BitVector::new(2).build();
         let _ = bv.access(2);
     }
 }
 
 #[cfg(test)]
 mod rank_success_tests {
+    use super::succinct::BitVector;
+
     macro_rules! parameterized_rank_tests {
         ($($name:ident: $value:expr,)*) => {
         $(
             #[test]
             fn $name() {
                 let (in_bv_str, in_i, expected_rank) = $value;
-                assert_eq!(succinct::BitVector::new(in_bv_str).rank(in_i), expected_rank);
+                assert_eq!(BitVector::new(in_bv_str).rank(in_i), expected_rank);
             }
         )*
         }
@@ -100,17 +116,19 @@ mod rank_success_tests {
 
 #[cfg(test)]
 mod rank_failure_tests {
+    use super::succinct::BitVector;
+
     #[test]
     #[should_panic]
     fn rank_over_lower_bound_causes_panic() {
-        let bv = succinct::BitVector::new(2).build();
+        let bv = BitVector::new(2).build();
         let _ = bv.rank(-1);
     }
 
     #[test]
     #[should_panic]
     fn rank_over_upper_bound_causes_panic() {
-        let bv = succinct::BitVector::new(2).build();
+        let bv = BitVector::new(2).build();
         let _ = bv.rank(2);
     }
 }
