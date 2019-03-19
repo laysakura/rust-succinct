e847cd515581d0c554121517fc7612555c463197
Sho Nakatani <lay.sakura@gmail.com>
2019-03-20 06:30:32 +0900

    adds tests for new(), build(), access()

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,59 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn build_works() {
+        let bv = succinct::BitVector::new(2);  // takes length
+        bv.build();
+        assert_eq!(bv.access(0), false);
+        assert_eq!(bv.access(1), false);
+    }
+
+    #[test]
+    fn without_build_works() {
+        let bv = succinct::BitVector::new(2);  // takes length
+        assert_eq!(bv.access(0), false);  // build() internally
+        assert_eq!(bv.access(1), false);
+    }
+
+    #[test]
+    #[should_panic]
+    fn access_over_lower_bound_causes_panic() {
+        let bv = succinct::BitVector::new(2).build();
+        let _ = bv.access(-1);
+    }
+
+    #[test]
+    #[should_panic]
+    fn access_over_upper_bound_causes_panic() {
+        let bv = succinct::BitVector::new(2).build();
+        let _ = bv.access(2);
+    }
+
+    #[test]
+    fn build_by_set_bit() {
+        let bv = succinct::BitVector::new(2)
+            .set_bit(1)
+            .build();
+        assert_eq!(bv.access(0), false);
+        assert_eq!(bv.access(1), true);
+    }
+
+    #[test]
+    fn build_by_str() {
+        let bv = succinct::BitVector::new("101").build();
+        assert_eq!(bv.access(0), true);
+        assert_eq!(bv.access(1), false);
+        assert_eq!(bv.access(2), true);
+    }
+
+    #[test]
+    fn build_by_str_with_set_bit() {
+        let bv = succinct::BitVector::new("101")
+            .set_bit(0)
+            .set_bit(1)
+            .build();
+        assert_eq!(bv.access(0), true);
+        assert_eq!(bv.access(1), true);
+        assert_eq!(bv.access(2), true);
     }
 }
