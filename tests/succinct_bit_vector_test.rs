e3db36ce944d4bb8ed492e2de2b3c2f52d7aeb15
Sho Nakatani <lay.sakura@gmail.com>
2019-03-25 01:04:01 +0900

    removes debug print

--- tests/succinct_bit_vector_test.rs
+++ tests/succinct_bit_vector_test.rs
@@ -40,7 +40,7 @@ fn build_from_str_and_set_bit() {
 
 #[test]
 fn rank_fuzzing_test() {
-    let samples = 10000;
+    let samples = 100000;
 
     fn access_from_str(s: &str, i: u64) -> bool {
         s.chars().collect::<Vec<char>>()[i as usize] == '1'
@@ -61,8 +61,6 @@ fn rank_fuzzing_test() {
         let bv = BitVectorBuilder::from_str(bvs).build();
 
         for i in 0.. s.len() {
-            println!("bit vec = {}, i={}", s, i);
-
             assert_eq!(bv.access(i as u64), access_from_str(s, i as u64),
                 "bit vec = {}, i={}, BitVector::access()={}, access_from_str={}",
                 s, i, bv.access(i as u64), access_from_str(s, i as u64));
