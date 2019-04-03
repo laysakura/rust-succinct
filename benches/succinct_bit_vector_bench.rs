b743a0d109d5933100ce5f30e0ea54da42b3d241
Sho Nakatani <lay.sakura@gmail.com>
2019-04-04 06:49:31 +0900

    parameterize N

--- benches/succinct_bit_vector_bench.rs
+++ benches/succinct_bit_vector_bench.rs
@@ -4,22 +4,27 @@ extern crate criterion;
 use criterion::Criterion;
 use succinct::{BitVectorBuilder, BitVectorString};
 
+const NS: [u64; 5] = [1 << 16, 1 << 17, 1 << 18, 1 << 19, 1 << 20];
+
 fn builder_from_length_benchmark(c: &mut Criterion) {
-    c.bench_function("BitVectorBuilder::from_length(2^20).build()", |b| {
-        b.iter(|| BitVectorBuilder::from_length(1 << 20).build())
-    });
+    c.bench_function_over_inputs(
+        "BitVectorBuilder::from_length(N).build()",
+        |b, &&n| b.iter(|| BitVectorBuilder::from_length(n).build()),
+        &NS,
+    );
 }
 
 fn builder_from_str_benchmark(c: &mut Criterion) {
-    c.bench_function(
-        "BitVectorBuilder::from_str(\"00...\" (2^20 \"0\"s)).build()",
-        |b| {
+    c.bench_function_over_inputs(
+        "BitVectorBuilder::from_str(\"00...(repeated N-times)\").build()",
+        |b, &&n| {
             b.iter(|| {
-                let s = String::from_utf8(vec!['0' as u8; 1 << 20]).unwrap();
+                let s = String::from_utf8(vec!['0' as u8; n as usize]).unwrap();
                 let bvs = BitVectorString::new(&s);
                 BitVectorBuilder::from_str(bvs).build()
             })
         },
+        &NS,
     );
 }
 
