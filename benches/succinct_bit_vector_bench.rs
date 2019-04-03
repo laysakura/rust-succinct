83fe1f2910b0ac21faff2d5c540baf70a399859b
Sho Nakatani <lay.sakura@gmail.com>
2019-04-04 07:28:16 +0900

    removes timing of setup

--- benches/succinct_bit_vector_bench.rs
+++ benches/succinct_bit_vector_bench.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::Criterion;
+use criterion::{BatchSize, Criterion};
 use succinct::{BitVectorBuilder, BitVectorString};
 
 const NS: [u64; 5] = [1 << 16, 1 << 17, 1 << 18, 1 << 19, 1 << 20];
@@ -18,11 +18,14 @@ fn builder_from_str_benchmark(c: &mut Criterion) {
     c.bench_function_over_inputs(
         "BitVectorBuilder::from_str(\"00...(repeated N-times)\").build()",
         |b, &&n| {
-            b.iter(|| {
-                let s = String::from_utf8(vec!['0' as u8; n as usize]).unwrap();
-                let bvs = BitVectorString::new(&s);
-                BitVectorBuilder::from_str(bvs).build()
-            })
+            b.iter_batched(
+                || {
+                    let s = String::from_utf8(vec!['0' as u8; n as usize]).unwrap();
+                    BitVectorString::new(&s)
+                },
+                |bvs| BitVectorBuilder::from_str(bvs).build(),
+                BatchSize::SmallInput,
+            )
         },
         &NS,
     );
@@ -31,6 +34,6 @@ fn builder_from_str_benchmark(c: &mut Criterion) {
 criterion_group!(
     benches,
     builder_from_length_benchmark,
-    builder_from_str_benchmark
+    builder_from_str_benchmark,
 );
 criterion_main!(benches);
