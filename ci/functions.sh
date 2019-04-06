47dfdbfbdbba48212c6343c2959d548ffd2d8114
Sho Nakatani <lay.sakura@gmail.com>
2019-04-06 12:24:49 +0900

    x

--- ci/functions.sh
+++ ci/functions.sh
@@ -1,8 +1,8 @@
-function downcase() { tr "[:upper:]" "[:lower:]"; }
-function upcase() { tr "[:lower:]" "[:upper:]"; }
+downcase() { tr "[:upper:]" "[:lower:]"; }
+upcase() { tr "[:lower:]" "[:upper:]"; }
 
-function ostype() { uname| downcase; }
-function is_linux() { [[ `ostype` == linux* ]]; }
-function is_osx() { [[ `ostype` == darwin* ]]; }
+ostype() { uname| downcase; }
+is_linux() { [[ `ostype` == linux* ]]; }
+is_osx() { [[ `ostype` == darwin* ]]; }
 
-function git_branch() { git symbolic-ref --short HEAD; }
+git_branch() { git symbolic-ref --short HEAD; }
