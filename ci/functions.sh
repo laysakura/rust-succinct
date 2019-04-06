a7207a15e0cb717b735ac24a7081826082980931
Sho Nakatani <lay.sakura@gmail.com>
2019-04-06 13:00:29 +0900

    x

--- ci/functions.sh
+++ ci/functions.sh
@@ -5,4 +5,10 @@ ostype() { uname| downcase; }
 is_linux() { [[ `ostype` == linux* ]]; }
 is_osx() { [[ `ostype` == darwin* ]]; }
 
-git_branch() { git symbolic-ref --short HEAD; }
+git_branch() {
+    if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+        echo -n $TRAVIS_BRANCH;
+    else
+        echo -n $TRAVIS_PULL_REQUEST_BRANCH;
+    fi
+}
