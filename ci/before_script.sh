2c2e13686f069205e3b221c5660a3ac70d52b37f
Sho Nakatani <lay.sakura@gmail.com>
2019-04-06 14:44:39 +0900

    x

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 set -eux
 
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-. $SCRIPT_DIR/functions.sh
-
 rustup component add rustfmt
--- ci/script.sh
+++ ci/script.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 set -eux
 
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-. $SCRIPT_DIR/functions.sh
-
 cargo build --release --verbose --all
 cargo test --release --verbose --all
 cargo fmt --all -- --check
 cargo doc
-find target/doc/
-cargo bench --all && rm -rf target/doc/criterion && mv -f target/criterion target/doc/
+cargo bench --all
+rm -rf target/doc/criterion && mv target/criterion target/doc/
